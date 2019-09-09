e1fac52b4ffac02446a7e7b6225a62ad5c411bb3
Nico Audy <nicoaudy1994@gmail.com>
2019-09-09 14:04:33 +0700

    ++ add auth middleware for manthra admin

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 
 $namespace = 'NicoAudy\LaravelManthra\Http\Controllers';
 
-Route::namespace($namespace)->prefix('manthra')->group(function () {
+Route::namespace($namespace)->prefix('manthra')->middleware('auth')->group(function () {
      Route::get('/', 'ManthraController@index');
      Route::post('/', 'ManthraController@store')->name('generate-manthra');
 });
